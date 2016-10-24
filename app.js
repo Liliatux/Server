e029470a7cf7548d51da73576d91aa7727a4d3dd
Lilia <geertszelia@gmail.com>
2016-10-24 14:30:22 +0200

    liste affichée - password IOT affiché

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 (function(){
-	console.log("hello");
+	'use strict';
+
+	var app = {
+
+		init: function() {
+			this.listeners();
+		},
+
+		listeners: function() {
+			$('button').on('click', this.start.bind(this));
+		},
+
+		start: function() {
+			var url = 'http://192.168.1.21:3000/places';
+			$.ajax(url)
+				.done(app.ajaxDone)
+				.fail(app.ajaxFail)
+				.always(app.ajaxAlways)	
+		},
+
+		ajaxDone: function(places) {
+			console.log(places);
+			var allPlaces = places.places;
+			console.log(allPlaces);
+			for (var i = 0; i < allPlaces.length; i++) {
+				$("ul").append("<li>" + allPlaces[i].nom + "</li>")
+			
+				if (allPlaces[i].nom === "IoT Valley") {
+					$("span").html(allPlaces[i].password);
+				}
+			}
+		}
+
+	}
+
+	app.init();
+	
 })();

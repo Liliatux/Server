f4ca5fe5fc3afbd64a9d9278726a28e2c9086bf2
Lilia <geertszelia@gmail.com>
2016-10-24 14:40:00 +0200

    step 0 OK (liste + password affichés)  - step 1 OpenWeather à faire

--- app.js
+++ app.js
@@ -14,26 +14,24 @@
 		start: function() {
 			var url = 'http://192.168.1.21:3000/places';
 			$.ajax(url)
-				.done(app.ajaxDone)
-				.fail(app.ajaxFail)
-				.always(app.ajaxAlways)	
+				.done(this.ajaxDone)
+				.fail(this.ajaxFail)
+				.always(this.ajaxAlways)	
 		},
 
 		ajaxDone: function(places) {
-			console.log(places);
 			var allPlaces = places.places;
-			console.log(allPlaces);
+
 			for (var i = 0; i < allPlaces.length; i++) {
 				$("ul").append("<li>" + allPlaces[i].nom + "</li>")
 			
 				if (allPlaces[i].nom === "IoT Valley") {
-					$("span").html(allPlaces[i].password);
+					$("#divPassword").html("Le mot de passe de l'IOT est :" + allPlaces[i].password);
 				}
 			}
 		}
-
 	}
 
 	app.init();
-	
+
 })();
